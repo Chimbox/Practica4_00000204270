2f0a28415073489250baed75af5553835bc116bf
chimbox <chimboxsoftgve@gmail.com>
2021-02-18 02:48:39 -0700

    Práctica número 4 terminada

---
+++ app/src/main/java/felix/alfonso/practica4_00000204270/MainActivity.kt
@@ -0,0 +1,18 @@
+package felix.alfonso.practica4_00000204270
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome_page.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_page)
+
+        btnGetStarted.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/felix/alfonso/practica4_00000204270/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package felix.alfonso.practica4_00000204270
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        btnSignIn.setOnClickListener {
+            val intent: Intent=Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
